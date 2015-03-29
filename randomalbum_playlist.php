dc2389e258d97e0d50f1ffd5cc0ef742f91f9f46
Niols <nicojpub@gmail.com>
2015-03-29 13:43:58 +0200

    Add warning and licence

--- randomalbum_playlist.php
+++ randomalbum_playlist.php
@@ -1,8 +1,18 @@
 <?php
 /******************************************************************************/
 /*                                                                            */
+/*  This file creates a playlist for Sonerezh <www.sonerezh.bzh> containing   */
+/*  all songs from on album in the database. You should be carefull while     */
+/*  using third-party scripts.                                                */
 /*                                                                            */
 /*                                                                            */
+/*  THE BEER-WARE LICENSE  (Revision 42):                                     */
+/*                                                                            */
+/*  Nicolas Jeannerod <niols@niols.net> wrote this file. As long as you       */
+/*  retain this notice you can do whatever you want with this stuff. If we    */
+/*  meet some day, and you think this stuff is worth it, you can buy me a     */
+/*  beer in return.                                                           */
+/*                                                      -- Poul-Henning Kamp  */
 /*                                                                            */
 /******************************************************************************/
 
