d1ec5989eb9fb7058a2a00c5612394926585457b
Niols <nicojpub@gmail.com>
2015-04-04 12:10:02 +0200

    Add clean_database file (as a reminder)

---
+++ clean_database.php
@@ -0,0 +1,17 @@
+<?php
+/******************************************************************************/
+/*                                                                            */
+/*  This file deletes from Sonerezh's database <www.sonerezh.bzh> that are    */
+/*  not anymore linked to an existing file. You should be carefull while      */
+/*  using third-party scripts.                                                */
+/*                                                                            */
+/*                                                                            */
+/*  THE BEER-WARE LICENSE  (Revision 42):                                     */
+/*                                                                            */
+/*  Nicolas Jeannerod <niols@niols.net> wrote this file. As long as you       */
+/*  retain this notice you can do whatever you want with this stuff. If we    */
+/*  meet some day, and you think this stuff is worth it, you can buy me a     */
+/*  beer in return.                                                           */
+/*                                                      -- Poul-Henning Kamp  */
+/*                                                                            */
+/******************************************************************************/
