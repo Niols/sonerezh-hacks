b7db1a9ff13cd1e86a24926135d38d475d5227cf
Niols <nicojpub@gmail.com>
2015-04-07 21:20:23 +0200

    Add array_column to init

--- inc/php-5.5/array_column.php
+++ inc/php-5.5/array_column.php
@@ -1,20 +1,17 @@
 <?php
 
-if (! function_exists ('array_column'))
+function array_column ($array, $column_key, $index_key = null)
 {
-  function array_column ($array, $column_key, $index_key = null)
+  try
   {
-    try
-    {
-      $output = array ();
-      foreach ($array as $key => $value)
-	$output [ $index_key === null ? $key : $value[$index_key] ] =
-          $column_key === null ? $value : $value[$column_key];
-      return $output;
-    }
-    catch (Exception $e)
-    {
-      return array ();
-    }
+    $output = array ();
+    foreach ($array as $key => $value)
+      $output [ $index_key === null ? $key : $value[$index_key] ] =
+        $column_key === null ? $value : $value[$column_key];
+    return $output;
+  }
+  catch (Exception $e)
+  {
+    return array ();
   }
 }
---
+++ init.php
@@ -0,0 +1,4 @@
+<?php
+
+if (! function_exists ('array_column'))
+  require 'inc/php-5.5/array_column.php';
