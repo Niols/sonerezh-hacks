99e6d7eeb0839b8769a3b7046a9cbf24079c18b1
Niols <nicojpub@gmail.com>
2015-04-07 08:49:44 +0200

    Remove songs that aren't in directory anymore

--- clean_database.php
+++ clean_database.php
@@ -15,3 +15,54 @@
 /*                                                      -- Poul-Henning Kamp  */
 /*                                                                            */
 /******************************************************************************/
+
+require 'inc/php-5.5/array_column.php';
+
+require 'inc/database_config.php';
+
+$conf = get_database_config ();
+
+$dsn = 'mysql:'
+     . 'dbname=' . $conf->database . ';'
+     . 'host=' . $conf->host . ';'
+     . 'charset=utf8';
+$pdo = new PDO ($dsn, $conf->login, $conf->password);
+
+function query_or_die ($sql, $die_message = 'Fatal error', $exec = false)
+{
+  global $pdo;
+  $s = $exec ? $pdo -> exec ($sql) : $pdo -> query ($sql);
+  if ($s === false)
+  {
+    $errorInfos = $pdo -> errorInfo ();
+    die ($die_message . ': ' . $errorInfos[2]);
+  }
+  return $exec ? true : $s -> fetchAll (PDO::FETCH_ASSOC);
+}
+
+echo 'Retrieving registered songs… ';
+$registered_songs = query_or_die ('SELECT source_path FROM songs');
+$registered_songs = array_column ($registered_songs, 'source_path');
+echo 'Found ' . count ($registered_songs) . ' songs.' . PHP_EOL;
+
+echo 'Retrieving present songs… ';
+exec ('find /home/music/music/', $real_songs);
+echo 'Found ' . count ($real_songs) . ' songs.' . PHP_EOL;
+
+echo 'Calculating songs to delete… ';
+$songs_to_delete = array_diff ($registered_songs, $real_songs);
+echo 'Found ' . count ($songs_to_delete) . ' songs.' . PHP_EOL;
+
+echo 'Deleting ' . count ($songs_to_delete) . ' songs. ';
+$sql = 'DELETE FROM songs WHERE source_path = "'
+     . implode ('" OR source_path = "', $songs_to_delete) . '"';
+query_or_die ($sql, 'Error while deleting songs', true);
+echo 'Done.' . PHP_EOL;
+
+/*
+echo 'Calculating songs to add… ';
+$songs_to_add = array_diff ($real_songs, $registered_songs);
+echo 'Found ' . count ($songs_to_add) . ' songs.' . PHP_EOL;
+*/
+
+print_r ($songs_to_add);
